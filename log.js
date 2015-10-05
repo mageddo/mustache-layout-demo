116556cea1b0373cbddff8fc6816eeab36d9c3ba
Elvis <edigitalb@gmail.com>
2015-10-04 22:44:19 -0300

    updating the demo

--- log.js
+++ log.js
@@ -1,4 +1,4 @@
-file:  /home/system/Dropbox/dev/testes/node-express-mustache-demo/002/view/index.html 
+file:  view/index.html 
 
 rootData:  { settings: 
    { 'x-powered-by': true,
@@ -9,14 +9,14 @@ rootData:  { settings:
      'trust proxy': false,
      'trust proxy fn': [Function: trustNone],
      view: [Function: View],
-     views: '/home/system/Dropbox/dev/testes/node-express-mustache-demo/002/view',
+     views: './view',
      'jsonp callback name': 'callback',
      'json spaces': 2,
      port: 3002,
      'view engine': 'html',
      'view options': { layout: true } },
   title: 'Demo mustache layout with express',
-  time: 'Sun Oct 04 2015 17:06:32 GMT-0300 (BRT)',
+  time: 'Sun Oct 04 2015 18:45:43 GMT-0300 (BRT)',
   _locals: [Function: locals],
   cache: false } 
 
@@ -25,7 +25,7 @@ next:  function (err, str){
     self.send(str);
   } 
 
-arguments:  { '0': '/home/system/Dropbox/dev/testes/node-express-mustache-demo/002/view/index.html',
+arguments:  { '0': 'view/index.html',
   '1': 
    { settings: 
       { 'x-powered-by': true,
@@ -36,39 +36,29 @@ arguments:  { '0': '/home/system/Dropbox/dev/testes/node-express-mustache-demo/0
         'trust proxy': false,
         'trust proxy fn': [Function: trustNone],
         view: [Function: View],
-        views: '/home/system/Dropbox/dev/testes/node-express-mustache-demo/002/view',
+        views: './view',
         'jsonp callback name': 'callback',
         'json spaces': 2,
         port: 3002,
         'view engine': 'html',
         'view options': [Object] },
      title: 'Demo mustache layout with express',
-     time: 'Sun Oct 04 2015 17:06:32 GMT-0300 (BRT)',
+     time: 'Sun Oct 04 2015 18:45:43 GMT-0300 (BRT)',
      _locals: [Function: locals],
      cache: false },
   '2': [Function] }
 this:  { name: 'index',
-  root: '/home/system/Dropbox/dev/testes/node-express-mustache-demo/002/view',
+  root: './view',
   defaultEngine: 'html',
   ext: '.html',
-  engine: [Function],
-  path: '/home/system/Dropbox/dev/testes/node-express-mustache-demo/002/view/index.html' } 
+  engine: { [Function] debug: [Function] },
+  path: 'view/index.html' } 
 --------------------------------------
 
 
-compilando com layout...
-compilando com layout e com view...
-renderizando...
- <html>
-	<head>
-		<title>{{title}}</title>
-	</head>
-	<body>
-		{{>body}}
-	</body>
-</html> 
----------------
- { settings: 
+file:  view/index.html 
+
+rootData:  { settings: 
    { 'x-powered-by': true,
      etag: 'weak',
      'etag fn': [Function: wetag],
@@ -77,13 +67,50 @@ renderizando...
      'trust proxy': false,
      'trust proxy fn': [Function: trustNone],
      view: [Function: View],
-     views: '/home/system/Dropbox/dev/testes/node-express-mustache-demo/002/view',
+     views: './view',
      'jsonp callback name': 'callback',
      'json spaces': 2,
      port: 3002,
      'view engine': 'html',
      'view options': { layout: true } },
   title: 'Demo mustache layout with express',
-  time: 'Sun Oct 04 2015 17:06:32 GMT-0300 (BRT)',
+  time: 'Sun Oct 04 2015 18:45:44 GMT-0300 (BRT)',
   _locals: [Function: locals],
-  cache: false }
+  cache: false } 
+
+next:  function (err, str){
+    if (err) return req.next(err);
+    self.send(str);
+  } 
+
+arguments:  { '0': 'view/index.html',
+  '1': 
+   { settings: 
+      { 'x-powered-by': true,
+        etag: 'weak',
+        'etag fn': [Function: wetag],
+        env: 'development',
+        'subdomain offset': 2,
+        'trust proxy': false,
+        'trust proxy fn': [Function: trustNone],
+        view: [Function: View],
+        views: './view',
+        'jsonp callback name': 'callback',
+        'json spaces': 2,
+        port: 3002,
+        'view engine': 'html',
+        'view options': [Object] },
+     title: 'Demo mustache layout with express',
+     time: 'Sun Oct 04 2015 18:45:44 GMT-0300 (BRT)',
+     _locals: [Function: locals],
+     cache: false },
+  '2': [Function] }
+this:  { name: 'index',
+  root: './view',
+  defaultEngine: 'html',
+  ext: '.html',
+  engine: { [Function] debug: [Function] },
+  path: 'view/index.html' } 
+--------------------------------------
+
+
